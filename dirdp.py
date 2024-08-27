cd9c2c13d827499ee3ae6cb76b96bdaa1246303c
Nicolò Rinaldi <nicolo@kali.kali_laptop>
2024-08-27 14:48:03 +0200

    Better thread management

--- dirdp.py
+++ dirdp.py
@@ -3,8 +3,7 @@ import os
 import sys
 import time
 import random
-import threading
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 GREEN = "\x1b[1;32m"
 RED = "\x1b[1;31m"
@@ -14,12 +13,10 @@ MAGENTA = "\x1b[1;35m"
 CYAN = "\x1b[1;36m"
 RESET = "\x1b[0m"
 
-
 OK = "\x1b[1;32m[\x1b[0m+\x1b[1;32m] "
 ERROR = "\x1b[1;31m[\x1b[0m!\x1b[1;31m] "
 WARNING = "\x1b[1;33m[\x1b[0m-\x1b[1;33m] "
 INFO = "\x1b[1;34m[\x1b[0mI\x1b[1;34m] "
-
 
 banner_ascii = """
    ccee88oo                    
@@ -33,31 +30,22 @@ CgggbU8OU qOp qOdoUOdcb          ____/ (_)_______/ /___
          |||\/                 
          |||||                 
    .....//||||\....            
-
 """
-
-max_threads = 0
-threads = 0
-event = threading.Event()
-
 
 def clean():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
-    
 
 def banner():
     colors = [GREEN, RED, YELLOW, BLUE, MAGENTA, CYAN]
     print(random.choice(colors) + banner_ascii + RESET)
 
-
 def checkArgs():
     if len(sys.argv) != 4:
-        print("\n" + ERROR + "Usage: python3 dirdp.py url worlist threads\n\nif you want max threads, set 0 to threads arg")
+        print("\n" + ERROR + "Usage: python3 dirdp.py url wordlist threads\n\nif you want max threads, set 0 to threads arg")
         sys.exit()
-
 
 def check():
     if "://" not in sys.argv[1]:
@@ -68,77 +56,44 @@ def check():
         print("\n" + ERROR + "Invalid domain")
         sys.exit()
     
-    if sys.argv[1].endswith("/") == False:
+    if not sys.argv[1].endswith("/"):
         sys.argv[1] += "/"
     
-    if os.path.isfile(sys.argv[2]) == False:
+    if not os.path.isfile(sys.argv[2]):
         print("\n" + ERROR + "Can't find wordlist " + sys.argv[2])
         sys.exit()
 
-
 def init():
-    global max_threads
-
     clean()
     banner()
     checkArgs()
     check()
 
-    max_threads = int(sys.argv[3])
-
+def fetch_url(word, url, http):
+    s = (url + word).replace("\n", "")
+    try:
+        response = http.request('GET', s)
+        if response.status < 400:
+            space = " " * (20 - len(word))
+            print(OK + (word + space + "(" + s + ")").replace("\n", ""))
+    except urllib3.exceptions.HTTPError as e:
+        pass
+    except Exception as e:
+        print(ERROR + f"An error occurred: {e}")
 
 def fuzzUrl():
-    global threads, max_threads
-
     url = sys.argv[1]
-    wordlist = open(sys.argv[2], "rt")
-
-    word = wordlist.readline()
+    max_threads = int(sys.argv[3]) or None
 
     start = time.time()
-
     http = urllib3.PoolManager()
 
-    while word != "":
-
-        if word.startswith("#") == False and word != "\n":
-
-            threading.Thread(target=find, args=(word, url, http,)).start()
-            
-            threads += 1
-
-            if threads == max_threads:
-                event.wait()
-                event.clear()
-
-        word = wordlist.readline()
-    
-    while threads != 0:
-        pass
+    with open(sys.argv[2], "rt") as wordlist, ThreadPoolExecutor(max_workers=max_threads) as executor:
+        for word in wordlist:
+            if not word.startswith("#") and word.strip():
+                executor.submit(fetch_url, word.strip(), url, http) 
 
     print("\n\n" + INFO + "search time: " + str(int(time.time() - start)) + " sec")
-
-
-def find(word, url, http: urllib3.PoolManager):
-    global threads
-
-    s = (url + word).replace("\n", "")
-
-    try:
-        code = http.request('GET', s).status
-
-        if code < 400:
-
-            space = " " * (20-len(word))
-
-            print(OK + (word + space + "(" + s + ")").replace("\n", ""))     
-        
-    except:
-        pass
-
-    threads -= 1
-    event.set()
-
 
 if __name__ == "__main__":
     init()
